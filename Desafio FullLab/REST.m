98e6cce6a06a6d9510e9174346a53e1cd96adabb
Brenno Ferrari <brennoferrari@gmail.com>
2017-06-21 10:37:23 -0300

    Criando métodos REST em um helper

--- Desafio FullLab/REST.m
+++ Desafio FullLab/REST.m
@@ -10,4 +10,87 @@
 
 @implementation REST
 
++ (void)GET:(NSString*)url
+     params:(NSDictionary*)params
+  onSuccess:(void (^)(NSURLSessionDataTask* task, id responseObject))success
+  onFailure:(void (^)(NSURLSessionDataTask* task, NSError* error))failure {
+    AFHTTPSessionManager* manager = [AFHTTPSessionManager new];
+    manager.securityPolicy.allowInvalidCertificates = YES;
+    
+    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    
+    [manager GET:url
+      parameters:params
+        progress:nil
+         success:success
+         failure:failure];
+}
+
++ (void)POST:(NSString*)url
+      params:(NSDictionary*)params
+   onSuccess:(void (^)(NSURLSessionDataTask* task, id responseObject))success
+   onFailure:(void (^)(NSURLSessionDataTask* task, NSError* error))failure {
+    AFHTTPSessionManager* manager = [AFHTTPSessionManager new];
+    manager.securityPolicy.allowInvalidCertificates = YES;
+    
+    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    
+    [manager POST:url
+       parameters:params
+         progress:nil
+          success:success
+          failure:failure];
+}
+
++ (void)PATCH:(NSString*)url
+       params:(NSDictionary*)params
+    onSuccess:(void (^)(NSURLSessionDataTask* task, id responseObject))success
+    onFailure:(void (^)(NSURLSessionDataTask* task, NSError* error))failure {
+    AFHTTPSessionManager* manager = [AFHTTPSessionManager new];
+    manager.securityPolicy.allowInvalidCertificates = YES;
+    
+    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    
+    [manager PATCH:url
+        parameters:params
+           success:success
+           failure:failure];
+}
+
++ (void)PUT:(NSString*)url
+     params:(NSDictionary*)params
+  onSuccess:(void (^)(NSURLSessionDataTask* task, id responseObject))success
+  onFailure:(void (^)(NSURLSessionDataTask* task, NSError* error))failure {
+    AFHTTPSessionManager* manager = [AFHTTPSessionManager new];
+    manager.securityPolicy.allowInvalidCertificates = YES;
+    
+    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    
+    [manager PUT:url
+      parameters:params
+         success:success
+         failure:failure];
+}
+
++ (void)DELETE:(NSString*)url
+        params:(NSDictionary*)params
+     onSuccess:(void (^)(NSURLSessionDataTask* task, id responseObject))success
+     onFailure:(void (^)(NSURLSessionDataTask* task, NSError* error))failure {
+    
+    AFHTTPSessionManager* manager = [AFHTTPSessionManager new];
+    manager.securityPolicy.allowInvalidCertificates = YES;
+    
+    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    
+    [manager DELETE:url
+         parameters:params
+            success:success
+            failure:failure];
+}
+
 @end
